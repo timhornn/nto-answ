94448340b55ba87d7a39e4348b22dfd8f0262031
timhornn <163546588+timhornn@users.noreply.github.com>
2024-09-21 10:08:23 +0700

    add gaed pencils
    A4 IS PEACE OF SHIT
    
    Signed-off-by: timhornn <163546588+timhornn@users.noreply.github.com>

---
+++ number_7.py
@@ -0,0 +1,4 @@
+official = float(input('Введите первое число: '))
+unofficial = float(input('Введите второе число: '))
+all_official = int(input('Введите третье число (ТОЛЬКО ЧИСЛО): ')) * 0.01
+print(all_official * official + (1 - all_official) * unofficial) if input("Влад А4 говно?\n").lower() == "да" else print("POSOSI")
